6432f1750b2a964e8c75244e4fca6b888c12366b
joakimolofsson <jjoakim.olofsson@gmail.com>
2025-06-04 11:46:12 +0200

    refactor: clarify startup instructions

--- Program.cs
+++ Program.cs
@@ -6,58 +6,70 @@ namespace DoubleStack
     {
         static void Main(string[] args)
         {
-			DoubleStack stack = new DoubleStack();
-			while (true)
-			{
-				if (stack.Depth == 0)
-				{
-					Console.WriteLine("Commands: q c + - * / number");
-					Console.WriteLine("[]");
-				}
-				else
-				{
-					Console.WriteLine(stack.ToString());
-				}
-				string input = Console.ReadLine().Trim();
-				if (input == "") input = " ";
-				char command = input[0];
-				if (Char.IsDigit(command))
-				{
-					double value = Convert.ToDouble(input);
-					stack.Push(value);
-				}
-				else if (command == '+')
-				{
-					stack.Push(stack.Pop() + stack.Pop());
-				}
-				else if (command == '*')
-				{
-					stack.Push(stack.Pop() * stack.Pop());
-				}
-				else if (command == '-')
-				{
-					double d = stack.Pop();
-					stack.Push(stack.Pop() - d);
-				}
-				else if (command == '/')
-				{
-					double d = stack.Pop();
-					stack.Push(stack.Pop() / d);
-				}
-				else if (command == 'c')
-				{
-					stack.Clear();
-				}
-				else if (command == 'q')
-				{
-					break;
-				}
-				else
-				{
-					Console.WriteLine("Illegal command, ignored");
-				}
-			}
-		}
+            RunProgram();
+        }
+
+        private static void RunProgram()
+        {
+            DoubleStack stack = new DoubleStack();
+
+            while (true)
+            {
+                if (stack.Depth == 0)
+                {
+                    Console.WriteLine("Commands: q c + - * / number");
+                    Console.WriteLine("Commands:\n" +
+                        "Quit = q\n" +
+                        "Show Commands = c\n");
+                    //Console.WriteLine("[]");
+                }
+                else
+                {
+                    Console.WriteLine(stack.ToString());
+                }
+
+                string input = Console.ReadLine().Trim();
+                if (input == "") input = " ";
+
+                char command = input[0];
+
+                if (Char.IsDigit(command))
+                {
+                    double value = Convert.ToDouble(input);
+                    stack.Push(value);
+                }
+                else if (command == '+')
+                {
+                    stack.Push(stack.Pop() + stack.Pop());
+                }
+                else if (command == '*')
+                {
+                    stack.Push(stack.Pop() * stack.Pop());
+                }
+                else if (command == '-')
+                {
+                    double d = stack.Pop();
+                    stack.Push(stack.Pop() - d);
+                }
+                else if (command == '/')
+                {
+                    double d = stack.Pop();
+                    stack.Push(stack.Pop() / d);
+                }
+                else if (command == 'c')
+                {
+                    stack.Clear();
+                }
+                else if (command == 'q')
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Illegal command, ignored");
+                }
+            }
+        }
     }
 
 	class DoubleStack
